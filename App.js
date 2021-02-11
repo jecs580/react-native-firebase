80d107aa4f887e2fd48388528dcdc534f2e66739
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-02-10 23:37:15 -0400

    Creacion del formulario para crear un usuario

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
+// Components
+import userListScreen from './screens/userListScreen';
+import CreateUserScreen from './screens/createUserScreen';
+import userDetailScreen from './screens/userDetailScreen';
+
+const Stack = createStackNavigator();
+function MyStack(){
+  return (
+    <Stack.Navigator>
+     <Stack.Screen name="CreateUserScreen" component={CreateUserScreen}/> 
+     <Stack.Screen name="UsersList" component={userListScreen}/> 
+     <Stack.Screen name="UserDetailScreen" component={userDetailScreen}/> 
+    </Stack.Navigator>
+  );
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/userDetailScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+const userDetailScreen = () => {
+    return (
+        <View>
+            <Text>User detail</Text>
+        </View>
+    )
+}
+
+export default userDetailScreen
